17941d40fe71bebdc69493ba8be0a87479b1f0e3
Vladimir Starkov <iamstarkov@gmail.com>
2015-07-24 03:06:30 +0300

    add implementation

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-export default item => item;
+import { isHeader, isLevel, match, text, html } from 'commonmark-helpers';
+import { partial, partialRight } from 'ramda';
+import trimTag from 'trim-html-tag';
+import moment from 'moment';
+
+const isDate = (pattern, locale, node) =>
+  moment(text(node), pattern, locale, true).isValid();
+
+const result = node => ({
+  text: text(node),
+  html: trimTag('p', html(node)),
+  sortable: new Date(text(node)).getTime(),
+  node
+});
+
+export default (pattern, locale, input) => {
+  const dateNode = match(input, partial(isDate, pattern, locale));
+  if (!dateNode) {
+    throw new Error(`Input has no date in given pattern '${pattern}' and locale '${locale}'`);
+  }
+  return result(dateNode);
+};
