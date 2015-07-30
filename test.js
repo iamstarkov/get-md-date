be0f3403a2acacaf22c8c9ca8518b623474ea16a
Vladimir Starkov <iamstarkov@gmail.com>
2015-07-30 14:12:56 +0200

    add link to related issue to the skipped test

--- test.js
+++ test.js
@@ -35,20 +35,8 @@ it('should throw an error if date not found', () => {
   }, /Input has no date in given pattern 'DD MM YYYY' and locale 'en'/);
 });
 
-// TODO: should work, but i’m doing smth wrong or it is a bug in moment:
-// var moment = require('moment')
-// moment('21 декабря 2015', 'DD MMM YYYY', 'ru', true).isValid();
-// TypeError: Cannot read property '0' of undefined
-//     at Locale.localeMonthsParse [as monthsParse] (/Users/matmuchrapna/projects/get-md-date/node_modules/moment/moment.js:654:49)
-//     at Object.<anonymous> (/Users/matmuchrapna/projects/get-md-date/node_modules/moment/moment.js:621:36)
-//     at addTimeToArrayFromToken (/Users/matmuchrapna/projects/get-md-date/node_modules/moment/moment.js:575:26)
-//     at configFromStringAndFormat (/Users/matmuchrapna/projects/get-md-date/node_modules/moment/moment.js:1192:17)
-//     at createFromConfig (/Users/matmuchrapna/projects/get-md-date/node_modules/moment/moment.js:1319:13)
-//     at createLocalOrUTC (/Users/matmuchrapna/projects/get-md-date/node_modules/moment/moment.js:1375:16)
-//     at local__createLocal (/Users/matmuchrapna/projects/get-md-date/node_modules/moment/moment.js:1379:16)
-//     at utils_hooks__hooks (/Users/matmuchrapna/projects/get-md-date/node_modules/moment/moment.js:16:29)
-//     at repl:1:1
-//     at REPLServer.defaultEval (repl.js:154:27)
+// TODO: moment crashs if you try to strictly validate locale dates #2527
+// https://github.com/moment/moment/issues/2527
 it.skip('should locale getDate', () => {
   equal(getDate('DD MMMM YYYY', 'ru', input).text, '21 Декабря 2015');
 });
