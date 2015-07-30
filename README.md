fa090c4a565eb827143a937932c69174fed3c111
Vladimir Starkov <iamstarkov@gmail.com>
2015-07-30 14:02:35 +0200

    updated testcases

--- README.md
+++ README.md
@@ -21,17 +21,17 @@ const input = `
 
 21 asd qwe
 
-21 *Dec* 2015
+22 *Dec* 2015
 
-21 December 2015
+23 December 2015
 
-21 Декабрь 2015
+24 Декабрь 2015
 `.trim();
 
 getDate('DD MMM YYYY',  'en', input).text;     // 21 Dec 2015
-getDate('DD MMM YYYY',  'en', input).html;     // 21 <em>Dec</em> 2015
+getDate('DD MMM YYYY',  'en', input).html;     // 22 <em>Dec</em> 2015
 getDate('DD MMM YYYY',  'en', input).sortable; // 1450645200000
-getDate('DD MMMM YYYY', 'en', input).text;     // 21 December 2015
+getDate('DD MMMM YYYY', 'en', input).text;     // 23 December 2015
 getDate('DD MM YYYY',   'en', input); // throw new Error:
                                       // Input has no date in given
                                       // pattern 'DD MM YYYY' and locale 'en'
--- test.js
+++ test.js
@@ -6,27 +6,27 @@ const input = `
 
 21 asd qwe
 
-21 *Dec* 2015
+22 *Dec* 2015
 
-21 December 2015
+23 December 2015
 
-21 Декабрь 2015
+24 Декабрь 2015
 `.trim();
 
 it('should text getDate', () => {
-  equal(getDate('DD MMM YYYY', 'en', input).text, '21 Dec 2015');
+  equal(getDate('DD MMM YYYY', 'en', input).text, '22 Dec 2015');
 });
 
 it('should html getDate', () => {
-  equal(getDate('DD MMM YYYY', 'en', input).html, '21 <em>Dec</em> 2015');
+  equal(getDate('DD MMM YYYY', 'en', input).html, '22 <em>Dec</em> 2015');
 });
 
 it('should sortable getDate', () => {
-  equal(getDate('DD MMM YYYY', 'en', input).sortable, 1450645200000);
+  equal(getDate('DD MMM YYYY', 'en', input).sortable, 1450738800000);
 });
 
 it('should getDate with with other pattern', () => {
-  equal(getDate('DD MMMM YYYY', 'en', input).text, '21 December 2015');
+  equal(getDate('DD MMMM YYYY', 'en', input).text, '23 December 2015');
 });
 
 it('should throw an error if date not found', () => {
