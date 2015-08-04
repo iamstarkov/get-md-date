3aad813fd56680313d1c871cb9c91e3ab924be5e
Vladimir Starkov <iamstarkov@gmail.com>
2015-08-04 23:53:13 +0200

    initial refactoring

--- index.js
+++ index.js
@@ -3,20 +3,21 @@ import { partial } from 'ramda';
 import trimTag from 'trim-html-tag';
 import moment from 'moment';
 
-const isDate = (pattern, locale, node) =>
-  moment(text(node), pattern, locale, true).isValid();
+const isDate = (format, locale, node) =>
+  moment(text(node), format, locale, true).isValid();
 
-const result = node => ({
+const result = (format, locale, node) => ({
   text: text(node),
   html: trimTag(html(node)),
-  sortable: new Date(text(node)).getTime(),
+  sortable: moment.utc(text(node), format, locale, true).unix(),
+  moment: moment(text(node), format, locale, true),
   node
 });
 
-export default (pattern, locale, input) => {
-  const dateNode = match(input, partial(isDate, pattern, locale));
-  if (!dateNode) {
-    throw new Error(`Input has no date in given pattern '${pattern}' and locale '${locale}'`);
+export default (format, locale, input) => {
+  const node = match(input, partial(isDate, format, locale));
+  if (!node) {
+    throw new Error(`Input has no date in given format '${format}' and locale '${locale}'`);
   }
-  return result(dateNode);
+  return result(format, locale, node);
 };
--- test.js
+++ test.js
@@ -22,17 +22,17 @@ it('should html getDate', () => {
 });
 
 it('should sortable getDate', () => {
-  equal(getDate('DD MMM YYYY', 'en', input).sortable > 1450000000000, true);
+  equal(getDate('DD MMM YYYY', 'en', input).sortable, 1450742400);
 });
 
-it('should getDate with with other pattern', () => {
+it('should getDate with with other format', () => {
   equal(getDate('DD MMMM YYYY', 'en', input).text, '23 December 2015');
 });
 
 it('should throw an error if date not found', () => {
   throws(() => {
     getDate('DD MM YYYY', 'en', input);
-  }, /Input has no date in given pattern 'DD MM YYYY' and locale 'en'/);
+  }, /Input has no date in given format 'DD MM YYYY' and locale 'en'/);
 });
 
 // TODO: moment crashs if you try to strictly validate locale dates #2527
